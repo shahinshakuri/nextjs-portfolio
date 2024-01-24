8d14e08765e5eab93fa1b577d1a115bd6e6bfb36
Shahin Shakuri <shahin_shakuri@ens.univ-artois.fr>
2024-01-25 00:58:53 +0100

    fix bug

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,9 +1,8 @@
-import Link from 'next/link';
 import React from 'react';
+import Image from 'next/image';
 import { AiOutlineMail } from 'react-icons/ai';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
-import Image from 'next/image';
 
 import ProfileImage from '../public/assets/sha.png';
 
@@ -15,7 +14,7 @@ const Main = () => {
           <div className='relative w-32 h-32 mx-auto mb-4'>
             <Image
               src={ProfileImage}
-              alt=''
+              alt='Profile Image'
               className='rounded-full object-cover'
               layout='fill'
               loading='lazy'
@@ -28,7 +27,7 @@ const Main = () => {
           </h1>
           <h1 className='py-2 text-gray-300'>Développeur Web</h1>
           <p className='py-4 text-gray-300 sm:max-w-[70%] m-auto'>
-            Je suis spécialisé dans la création d'applications web tout en
+            Je suis spécialisé dans la création d&apos;applications web tout en
             suivant une formation en alternance.
           </p>
         </div>
