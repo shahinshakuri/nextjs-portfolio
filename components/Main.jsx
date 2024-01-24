7e8bcda86798f07d3c6f17ff331bc8378b84f6e0
Shahin Shakuri <shahin_shakuri@ens.univ-artois.fr>
2024-01-24 23:45:05 +0100

    fix bug

--- components/Main.jsx
+++ components/Main.jsx
@@ -23,11 +23,13 @@ const Main = () => {
           </div>
 
           <h1 className='py-4 text-gray-300'>
-            Salut, je suis <span className='text-[#727274]'>Shahin</span>
+            Salut, je suis{' '}
+            <span className='text-[#727274]'>Shahin</span>
           </h1>
           <h1 className='py-2 text-gray-300'>Développeur Web</h1>
           <p className='py-4 text-gray-300 sm:max-w-[70%] m-auto'>
-            Je suis spécialisé dans la création d'applications web tout en suivant une formation en alternance.
+            Je suis spécialisé dans la création d'applications web tout en
+            suivant une formation en alternance.
           </p>
         </div>
       </div>
